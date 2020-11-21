383c3aebee48d9d6eae71f67e075121ccb2b4b38
ait <ait@ex.in>
2020-11-21 23:25:03 +0900

    [routing] 動的ルーティング出し分け

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <p>pages/users/_id.vue</p>
+    <p>{{ userId }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      /*
+       * ユーザーID は ルーターのパラーメータのID
+       * /users/xxx のxxxを表示に返す
+       */
+      userId: this.$route.params.id,
+    }
+  },
+}
+</script>
