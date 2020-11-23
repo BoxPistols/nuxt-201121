7d5ca83d86078878aaaf7b8803c6654f2c702965
ait <ait@ex.in>
2020-11-23 21:34:41 +0900

    set axios

--- pages/users/_usersid.vue
+++ pages/users/_usersid.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>pages/users/_id.vue</p>
+    <p>pages/users/_usersid.vue</p>
     <p>{{ userId }}</p>
   </div>
 </template>
@@ -13,7 +13,7 @@ export default {
        * ユーザーID は ルーターのパラーメータのID
        * /users/xxx のxxxを表示に返す
        */
-      userId: this.$route.params.id,
+      userId: this.$route.params.usersid,
     }
   },
 }
